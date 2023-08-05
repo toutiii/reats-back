59dfd9abdc7a69bf33565068ef001523e5d9fd4c
Toutel <twotell75@gmail.com>
2023-08-05 18:57:13 +0000

    Resolve "install-pre-commit"

--- reats/source/client_app/apps.py
+++ reats/source/client_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ClientAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'client_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "client_app"
--- reats/source/cooker_app/apps.py
+++ reats/source/cooker_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CookerAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cooker_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cooker_app"
