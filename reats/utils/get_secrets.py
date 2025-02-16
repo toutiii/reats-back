2177aa1d8210d278a350d34723fc589258d9f275
tout_yong <twotell75@gmail.com>
2025-02-16 16:49:42 +0100

    Simple way to deal with local dev env

--- reats/utils/get_secrets.py
+++ reats/utils/get_secrets.py
@@ -12,9 +12,13 @@ def fetch_aws_secrets(secret_name: str):
         raise ValueError("AWS_REGION environment variable is not set")
 
     if os.environ["ENV"] == "local":
-        session = boto3.session.Session(profile_name=os.environ["AWS_PROFILE"])
+        session = boto3.Session(
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+            region_name=region,
+        )
     else:
-        session = boto3.session.Session()
+        session = boto3.Session()  # Uses IAM Role in AWS
 
     secrets_client = session.client("secretsmanager", region_name=region)
 
