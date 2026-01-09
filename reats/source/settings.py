a185648dc0180753a8215e43e908f49f895425af
Joel Nguemeta <joeledmond95@gmail.com>
2026-01-09 13:57:41 +0100

    feature: add django_filter

--- reats/source/settings.py
+++ reats/source/settings.py
@@ -59,7 +59,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
+    "django_filters"
+
     "customer_app",
     "cooker_app",
     "delivery_app",
