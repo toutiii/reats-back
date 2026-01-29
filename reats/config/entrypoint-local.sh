a2b22c6ec381cbee98111dfc6c4c537f6dc3ba45
tout_yong <twotell75@gmail.com>
2026-01-29 20:35:28 +0100

    Splitting local conf and remote one

--- reats/config/entrypoint-local.sh
+++ reats/config/entrypoint-local.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+export ENV='local'
+
+echo "Local environment detected. Waiting for PostgreSQL..."
+./wait-for-it.sh -h db -p 5432 -t 30 -- echo "PostgreSQL is up"
 
 # Django setup
 python manage.py flush --no-input
@@ -8,4 +12,4 @@ python manage.py migrate
 python manage.py loaddata */fixtures/*.json
 
 # Start Gunicorn
-gunicorn --bind 0.0.0.0:8000 source.wsgi
+gunicorn --reload --bind 0.0.0.0:8000 source.wsgi
