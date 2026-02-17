d2c6aae21087c98baa6e7b4bbfb4b30cc3d8d4ab
proxidev <joeledmond95@gmail.com>
2026-02-17 09:41:18 +0100

    feat: implement init_data management command for database seeding and update entrypoint (#28)

--- reats/config/entrypoint.sh
+++ reats/config/entrypoint.sh
@@ -6,6 +6,7 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata */fixtures/*.json
+python manage.py init_data
 
 # Start Gunicorn
 gunicorn --bind 0.0.0.0:8000 source.wsgi
