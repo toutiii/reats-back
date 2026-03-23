22e4bc0fa51b831d87775d1e1cd314dab247662b
Joel Nguemeta <joeledmond95@gmail.com>
2026-03-23 12:21:50 +0100

    feat: update DrinkView for multi-image support

--- reats/cooker_app/views.py
+++ reats/cooker_app/views.py
@@ -827,6 +827,7 @@ class DrinkView(StandardizedResponseMixin, ModelViewSet):
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
+        serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
 
         if getattr(instance, "_prefetched_objects_cache", None):
