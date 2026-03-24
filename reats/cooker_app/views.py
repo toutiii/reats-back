022427ccba7d24f44b454a23f65a4e58ee782fc2
Joel Nguemeta <joeledmond95@gmail.com>
2026-03-24 12:07:46 +0100

    refactor(cooker_app): simplify photos key extraction in DrinkView

--- reats/cooker_app/views.py
+++ reats/cooker_app/views.py
@@ -852,6 +852,7 @@ class DrinkView(StandardizedResponseMixin, ModelViewSet):
         current_object = self.get_object()
         cooker_pk = str(current_object.cooker.pk)
         photos = self.request.FILES.getlist("photos")
+        photos = self.request.FILES.getlist("photos")
 
         if photos:
             # Replace all existing images with the newly uploaded ones
